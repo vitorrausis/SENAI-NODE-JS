bfb7f8bdd82999994b01a03684edaee082c7001a
Luciano Giusti <121564508+Giusti231@users.noreply.github.com>
2023-08-15 14:51:18 -0300

    alterando arquivos

--- Luciano_Giusti/Projeto_Hello_World/Projeto_File/lerArquivo.js
+++ Luciano_Giusti/Projeto_Hello_World/Projeto_File/lerArquivo.js
@@ -1,5 +1,21 @@
-import * as fs from 'fs/promises';
+var fs = require('fs');
 fs.appendFile('novo.txt', 'Olá NodeJS! SENAI', function (err){
     if (err) throw err;
     console.log('Arquivo salvo!');
+});
+
+fs. writeFile('novo.txt', 'Olá NodeJS! UNISENAI 2023', function (err){
+    if (err) throw err;
+    console.log('Arquivo.txt alterado!');
+
+});
+
+fs.rename('novo.txt', 'ArquivoNovoRenomeado.txt', function(err){
+    if(err) throw err;
+    console.log('Arquivo renomeado!');
+});
+
+fs.unlink('ArquivoNovoRenomeado.txt', function(err){
+    if(err) throw err;
+    console.log('Arquivo deletado!');
 });
