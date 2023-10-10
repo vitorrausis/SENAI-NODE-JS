e5d18338c9f1f79399f1c2aeb68ba248b6145b88
leogmay <125558569+leogmay@users.noreply.github.com>
2023-10-09 21:26:20 -0300

    LeonardoMay
    Criando pasta de arquivos.

---
+++ Leonardo/CalculadoraNode/calc.js
@@ -0,0 +1,9 @@
+let SomaFunc = require("./somar");
+let DivideFunc = require("./dividir");
+let MultiplicaFunc = require("./multiplicar");
+let SubtrairFunc = require("./subtrair");
+
+console.log(`O valor da soma é: ${SomaFunc(1,2)}`);
+console.log(`O valor da divisão é: ${DivideFunc(1,2)}`);
+console.log(`O valor da multiplicação é: ${MultiplicaFunc(1,2)}`);
+console.log(`O valor da subtração é: ${SubtrairFunc(1,2)}`);
---
+++ Leonardo/CalculadoraNode/subtrair.js
@@ -0,0 +1,5 @@
+const subtrai = function(a,b){
+    return a-b;
+
+}
+module.exports=subtrai;
