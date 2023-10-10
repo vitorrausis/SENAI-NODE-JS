0035c5d7f1449798626e1774bfe6f9fc6b3c555d
sabbajohn <victorsabba@gmail.com>
2023-10-10 13:47:20 -0300

    OT-05

---
+++ sabbajohn/OT-05/ListaDeGames/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+let games = [
+    {title:"Sea of Theeves", studio:"Rare", price:30},
+    {title:"WoW", studio:"Blizzard", price:120},
+    {title:"Valorant", studio:"Riot", price:0},
+    {title:"COD", studio:"Activision", price:200},
+    {title:"Minecraft", studio:"Mojang", price:80},
+    {title:"Halo",  studio:"Microsoft", price:90},
+    {title:"Black",  studio:"Eletronic Arts", price:120},
+    {title:"FIFA",  studio:"Eletronic Arts", price:100},
+    {title:"Battlefield 1",  studio:"Dice", price:300},
+    {title:"Battlefield 4",  studio:"Dice", price:150},
+    {title:"Battlefield 5",  studio:"Dice", price:350},
+]
