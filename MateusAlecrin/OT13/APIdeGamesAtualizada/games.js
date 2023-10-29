b8f985b858309e405aec7b109d0da4dcc967a0e0
mateusalecrin <mateus.alecrin@hotmail.com>
2023-10-29 15:31:59 -0300

    :sparkles: feat(new exercises): OTs 10|11|12|13

---
+++ MateusAlecrin/OT13/APIdeGamesAtualizada/games.js
@@ -0,0 +1,23 @@
+const colecaoGames = [
+    {id: 1,  title: "Assassin's Creed Odyssey",        age:"431 - 404 AC",   studio: "Ubisoft Quebec",   price: 199.00},
+    {id: 2,  title: "Assassin's Creed Origins",        age:"49 - 43 AC",     studio: "Ubisoft Montreal", price: 199.00},
+    {id: 3,  title: "Assassin's Creed Valhalla",       age:"873 DC",         studio: "Ubisoft Montreal", price: 279.95},
+    {id: 4,  title: "Assassin's Creed",                age:"1191 DC",        studio: "Ubisoft Montreal", price: 90},
+    {id: 5,  title: "Assassin's Creed II",             age:"1476 - 1499 DC", studio: "Ubisoft Montreal", price: 90},
+    {id: 6,  title: "Assassin's Creed Revelations",    age:"1511 DC",        studio: "Ubisoft",          price: 90},
+    {id: 7,  title: "Assassin's Creed IV Black Flag",  age:"1712 - 1722 DC", studio: "Ubisoft Montreal", price: 99.00},
+    {id: 8,  title: "Assassin's Creed Rogue",          age:"1756 - 1763 DC", studio: "Ubisoft Sofia",    price: 79.00},
+    {id: 9,  title: "Assassin's Creed III",            age:"1754 - 1783 DC", studio: "Ubisoft",          price: 90},
+    {id: 10, title: "Assassin's Creed III Liberation", age:"1765 - 1780 DC", studio: "Ubisoft",          price: 90},
+    {id: 11, title: "Assassin's Creed Unity",          age:"1776 - 1800 DC", studio: "Ubisoft Montreal", price: 99.00},
+    {id: 12, title: "Assassin's Creed Syndicate",      age:"1868 DC",        studio: "Ubisoft Quebec",   price: 89.95},
+    {id: 13, title: "Assassin's Creed Brotherhood",    age:"1500 - 1507 DC", studio: "Ubisoft",          price: 90},
+    {id: 14, title: "Super Mario Bros",                age:"1985",           studio: "Nintendo Research & Development 4",                 price: 185.3},
+    {id: 15, title: "Super Mario Bros 3",              age:"1988",           studio: "Nintendo Entertainment Analysis & Development",     price: 175.3},
+    {id: 16, title: "Super Mario World",               age:"1990",           studio: "Nintendo Entertainment Analysis & Development",     price: 165.3},
+    {id: 17, title: "Super Mario 64",                  age:"1996",           studio: "Nintendo Entertainment Analysis & Development",     price: 155.3},
+    {id: 18, title: "Super Mario Galaxy",              age:"2007",           studio: "Nintendo Entertainment Analysis & Development",     price: 145.3},
+    {id: 19, title: "Super Mario Odyssey",             age:"2017",           studio: "Nintendo's Entertainment Planning & Development",   price: 135.3}
+];
+
+export default colecaoGames;
