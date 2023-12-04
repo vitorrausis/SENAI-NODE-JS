4378c9fa95ef86fd51165944ea05ebfbc388d314
CarloLaurindo1 <laurindo.ale@gmail.com>
2023-12-04 14:55:06 -0300

    Change: Atualizações: FInalizando ultimas OT's, refatoração, e todolist parte 1 e 2

--- CarlosLaurindo/ead_24nov23/todolist/index.js
+++ CarlosLaurindo/ead_24nov23/todolist/index.js
@@ -1,27 +1,41 @@
+
 const express = require('express');
+var bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public',express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
+
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/', (req,res) =>{
+    tasks.push(req.body.task);
+    res.render('index', {tasksList: tasks});
+})
 
 let tasks = ['Passear com o dog', 'Ir ao mercado', 'Comprar pão'];
 
-app.get('/', (req, res) => {
-    res.render('index', { tasksList: tasks });
-})
+app.get('/', (req,res) =>{
+    res.render('index',{tasksList: tasks});
+});
 
-app.get('/deletar/:id', (req, res) => {
-    tasks = tasks.filter(function (val, index) {
-        if (index != req.params.id) {
+app.get('/deletar/:id', (req, res) =>{
+    tasks = tasks.filter(function(val,index){
+        if (index!= req.params.id){
             return val;
         }
-    })
-    res.render('index', { tasksList: tasks });
+        res.redirect('/');
+    })  
+    res.render('index',{tasksList:tasks});
 })
 
-app.listen(5000, () => {
+
+app.listen(5000, ()=>{
     console.log('Servidor rodando em http://localhost:5000');
 })
