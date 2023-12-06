71436695b3718e20e39adaa23af95916eb538ec0
MATHEUS WILLIAN SCHMOELLER TRINDADE <matheus_w_trindade@estudante.sesisenai.org.br>
2023-12-06 19:32:06 -0300

    Atividades calculadora e listadegames

---
+++ Matheus_Trindade/CalculadoraNode/calc.js
@@ -0,0 +1,13 @@
+let SomaFunc = require("./somar").soma;
+console.log(`O valor da soma é: ${SomaFunc(1, 2)}`);
+
+let SubFunc = require("./somar").sub;
+console.log(`O valor da subtração é: ${SubFunc(10, 5)}`);
+
+
+let MultFunc = require("./somar").mult;
+console.log(`O valor da multiplicação é: ${MultFunc(10,2)}`);
+
+let DivFunc = require("./somar").div;
+console.log(`O valor da divisão é: ${DivFunc(10,2)}`);
+
