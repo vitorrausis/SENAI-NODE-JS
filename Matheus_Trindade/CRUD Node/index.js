672a4772aa09341ea78bba6b19936c1eb4ec7298
MATHEUS WILLIAN SCHMOELLER TRINDADE <matheus_w_trindade@estudante.sesisenai.org.br>
2023-12-07 18:46:53 -0300

    CRUD atividade

---
+++ Matheus_Trindade/CRUD Node/index.js
@@ -0,0 +1,46 @@
+import Express from 'Express';
+
+const app = Express();
+
+var carros = ['fiesta', 'saveiro'];
+
+app.use(Express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => 
+    res.send("<h3>Rotas no Express</h3><p>Rota '/'")
+) 
+
+app.get("/sobre", (req, res) => 
+    res.send("<h3>Teste</h3><p>Rota '/'")
+) 
+
+app.get("/users/:name", (req, res) => 
+    res.send("Usuário:" + req.params.name)
+);
+
+app.post('/cars/', (req, res) => {
+    let name = req.body.name;
+    carros[(carros.length)] = name;
+    return res.json([carros[(carros.length - 1)]]);
+});
+
+app.get('/cars/:id', (req, res) => {
+    let id = req.params.id;
+    return res.json([carros[id]])
+});
+
+app.put('/cars/update/:id', (req, res) =>{
+    let name = req.body.name;
+    carros[req.params.id] = name;
+    return res.json(carros[req.params.id]);
+});
+
+app.delete('/cars/delete/:id', (req, res) => {
+    let id = req.params.id;
+    carros[id] = null;
+    return res.json(carros[id]);
+})
+
+app.listen(3080, () =>{
+    console.log("Servidor rodando!");
+})
