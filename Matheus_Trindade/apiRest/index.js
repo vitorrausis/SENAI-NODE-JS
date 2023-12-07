b579b875b67788d9e0ff88fa582c70a67d1fd3c9
MATHEUS WILLIAN SCHMOELLER TRINDADE <matheus_w_trindade@estudante.sesisenai.org.br>
2023-12-07 20:14:45 -0300

    Update ApiREST

--- Matheus_Trindade/apiRest/index.js
+++ Matheus_Trindade/apiRest/index.js
@@ -1,15 +1,11 @@
 import express from 'express';
-import colecaoUf from './dados/dados.js';
+import { buscarUfs, buscarUfsPorId, buscarUfsPorNome } from './servicos/servico.js';
 
 const app = express();
 
-const buscarUfsPorNome = (nomeUf) => {
-    return colecaoUf.filter(uf => uf.nome.toLowerCase().includes(nomeUf.toLowerCase()));
-};
-
 app.get('/ufs', (req, res) => {
     const nomeUf = req.query.busca;
-    const resultado = nomeUf ? buscarUfsPorNome(nomeUf) : colecaoUf;
+    const resultado = nomeUf ? buscarUfsPorNome(nomeUf) : buscarUfs();
     if (resultado.length > 0) {
         res.json(resultado);
     } else {
@@ -18,23 +14,13 @@ app.get('/ufs', (req, res) => {
 });
 
 app.get('/ufs/:iduf', (req, res) => {
-    const idUF = parseInt(req.params.iduf);
-    let mensagemErro = '';
-    let uf;
-
-    if (!(isNaN(idUF))) {
-        uf = colecaoUf.find(u => u.id === idUF);
-        if(!uf) {
-            mensagemErro = 'UF não encontrada';
-        }
-    } else {
-        mensagemErro = 'Requisição inválida';
-    }
-
+    const uf = buscarUfsPorId(req.params.iduf);
     if (uf) {
         res.json(uf);
+    } else if (isNaN(parseInt(req.params.iduf))) {
+        res.status(400).send({ "erro": "Requisição Inválida" });
     } else {
-        res.status(404).send({ "erro": mensagemErro });
+        res.status(404).send({ "erro": "UF não encontrada" });
     }
 });
 
