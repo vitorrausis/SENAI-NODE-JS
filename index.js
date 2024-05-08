6487f430418ff02a58a1b55ebb3b98e44616edef
vitorrausis <vitor_rausis@estudante.sc.senai.br>
2024-05-08 20:51:14 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var fs  = require("fs");
+const { error } = require("console");
+
+http.createServer(function(request, response ){
+    fs.readFile("index.html", function(erro, conteudo){
+        if(erro){
+            console.log(erro);
+        }else{
+            response.write(conteudo);
+        }
+        response.end();
+    })
+}).listen(8081);
+
+console.log("Servidor rodando em http://localhost:8081");
